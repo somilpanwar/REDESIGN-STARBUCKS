ef48817c62b15103ac4d1e1493f08c3b2e2ae27e
somil panwar <122174329+somilpanwar@users.noreply.github.com>
2024-08-13 00:31:23 +0530

    gift page completed

--- starbucks/src/components/Gift.js
+++ starbucks/src/components/Gift.js
@@ -25,10 +25,32 @@ const Gift = () => {
           <button>Order Now</button>
         </div>
       </div>
-      <div className="selectionbar">
-        <button >ANYTHING</button>
-        <button >THANKYOU</button>
-        <button >CONGRATS</button>
+      <div className="selectionbargift">
+        <button>ANYTHING</button>
+        <button>THANKYOU</button>
+        <button>CONGRATS</button>
+      </div>
+
+      <div className='arrangeitems'>
+        <div className='cardgift'>
+          <img src='https://preprodtsbstorage.blob.core.windows.net/cms/uploads/TSB_GC_indiacard_1_1_28dafb2bb6.png'/>
+          <h2>India Exclusive</h2>
+          <p>Bring in the festive season and make each celebration memorable.</p>
+        <button>Add Item</button>
+        </div>
+        <div className='cardgift'>
+        <img src='https://preprodtsbstorage.blob.core.windows.net/cms/uploads/71d3780c_be6e_46b1_ab01_8a2bce244a7f_1_1_2d1afadaa0.png'/>
+        <h2>Starbucks Coffee</h2>
+          <p>Starbucks is best when shared. Treat your pals to a good cup of coffee.</p>
+        <button>Add Item</button>
+        </div>
+        <div className='cardgift'>
+        <img src='https://preprodtsbstorage.blob.core.windows.net/cms/uploads/97ee3280_2f05_43ad_bd94_f5c184d4f502_1_0a31455af9.png'/>
+        <h2>
+        U Keep Me Warm</h2>
+          <p>Captivating, cosy, coffee. Gift your loved ones this Starbucks Gift Card</p>
+        <button>Add Item</button>
+        </div>
       </div>
 
       <Footer/>
